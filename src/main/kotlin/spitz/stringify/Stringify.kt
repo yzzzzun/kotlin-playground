9642cf1e427d64187528a06f5a8b597782ff4f2b
yzzzzun <yzzzzun@gmail.com>
2023-04-21 01:38:15 +0900

    refactor: StringBuilder의 확장함수로 jsonStringify 구현

--- src/main/kotlin/spitz/stringify/Stringify.kt
+++ src/main/kotlin/spitz/stringify/Stringify.kt
@@ -8,38 +8,39 @@ fun main() {
     println(stringify(Json1(3, "abc", listOf("1", "2", "3"))))
 }
 
-fun jsonValue(value: Any?, builder: StringBuilder) {
+fun StringBuilder.jsonValue(value: Any?) {
     when (value) {
-        null -> builder.append("null")
-        is String -> jsonString(value, builder)
-        is Boolean, is Number -> builder.append("$value")
-        is List<*> -> jsonList(value, builder)
-        else -> jsonObject(value, builder)
+        null -> append("null")
+        is String -> jsonString(value)
+        is Boolean, is Number -> append("$value")
+        is List<*> -> jsonList(value)
+        else -> jsonObject(value)
     }
 }
 
-fun stringify(value: Any): String {
-    val builder = StringBuilder()
-    jsonValue(value, builder)
-    return builder.toString()
+fun stringify(value: Any) = StringBuilder().run {
+    jsonValue(value)
+    toString()
 }
 
-private fun jsonString(v: String, builder: StringBuilder) = builder.append(""""${v.replace("\"", "\\\"")}"""")
-private fun jsonList(target: List<*>, builder: StringBuilder) {
-    builder.append('[')
-    target.joinTo({ builder.append(',') }) {
-        jsonValue(it, builder)
-    }
-    builder.append(']')
-}
-
-private fun <T : Any> jsonObject(target: T, builder: StringBuilder) {
-    builder.append('{')
-    target::class.members.filterIsInstance<KProperty<*>>()
-        .joinTo({ builder.append(',') }) {
-            "${jsonValue(it.name, builder)}:${jsonValue(it.getter.call(target), builder)}"
+private fun StringBuilder.jsonString(v: String) = append(""""${v.replace("\"", "\\\"")}"""")
+private fun StringBuilder.jsonList(target: List<*>) {
+    wrap('[', ']') {
+        target.joinTo(::comma) {
+            jsonValue(it)
         }
-    builder.append('}')
+    }
+}
+
+private fun <T : Any> StringBuilder.jsonObject(target: T) {
+    wrap('{', '}') {
+        target::class.members.filterIsInstance<KProperty<*>>()
+            .joinTo(::comma) {
+                jsonValue(it.name)
+                append(":")
+                jsonValue(it.getter.call(target))
+            }
+    }
 }
 
 fun <T> Iterable<T>.joinTo(sep: () -> Unit, transform: (T) -> Unit) {
@@ -47,6 +48,16 @@ fun <T> Iterable<T>.joinTo(sep: () -> Unit, transform: (T) -> Unit) {
         if (count != 0) sep()
         transform(element)
     }
+}
+
+fun StringBuilder.comma() {
+    append(',')
+}
+
+fun StringBuilder.wrap(begin: Char, end: Char, block: StringBuilder.() -> Unit) {
+    append(begin)
+    block()
+    append(end)
 }
 
 class Json0(val a: Int, val b: String)
