ae148ceee35467fa8593ff88dd2432c4b18fbff1
yzzzzun <yzzzzun@gmail.com>
2023-04-21 02:58:26 +0900

    feat: annotation 적용

--- src/main/kotlin/spitz/stringify/Stringify.kt
+++ src/main/kotlin/spitz/stringify/Stringify.kt
@@ -1,11 +1,13 @@
 package spitz.stringify
 
 import kotlin.reflect.KProperty
+import kotlin.reflect.full.findAnnotation
 
 
 fun main() {
     println(stringify(Json0(3, "abc")))
     println(stringify(Json1(3, "abc", listOf("1", "2", "3"))))
+    println(stringify(Json2(3, "abc")))
 }
 
 fun StringBuilder.jsonValue(value: Any?) {
@@ -35,8 +37,9 @@ private fun StringBuilder.jsonList(target: List<*>) {
 private fun <T : Any> StringBuilder.jsonObject(target: T) {
     wrap('{', '}') {
         target::class.members.filterIsInstance<KProperty<*>>()
+            .filter { it.findAnnotation<Ex>() == null }
             .joinTo(::comma) {
-                jsonValue(it.name)
+                jsonValue(it.findAnnotation<Name>()?.name ?: it.name)
                 append(":")
                 jsonValue(it.getter.call(target))
             }
@@ -62,3 +65,10 @@ fun StringBuilder.wrap(begin: Char, end: Char, block: StringBuilder.() -> Unit) 
 
 class Json0(val a: Int, val b: String)
 class Json1(val a: Int, val b: String, val c: List<String>)
+class Json2(@Ex val a: Int, @Name("title") val b: String)
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class Ex
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class Name(val name: String)
